48e61379a818ce87e2e55ad16e16e10c5def7e0b
Vanč Levstik <vanc.levstik@gmail.com>
2014-10-06 16:41:04 +0200

    Add support for HP Cloud DNS management

--- libcloud/common/openstack.py
+++ libcloud/common/openstack.py
@@ -131,7 +131,9 @@ class OpenStackBaseConnection(ConnectionUserAndKey):
                  ex_force_service_type=None,
                  ex_force_service_name=None,
                  ex_force_service_region=None,
-                 retry_delay=None, backoff=None):
+                 retry_delay=None, backoff=None
+                 ex_domain_name=None):
+
         super(OpenStackBaseConnection, self).__init__(
             user_id, key, secure=secure, timeout=timeout,
             retry_delay=retry_delay, backoff=backoff, proxy_url=proxy_url)
@@ -146,6 +148,8 @@ class OpenStackBaseConnection(ConnectionUserAndKey):
         self._ex_force_service_type = ex_force_service_type
         self._ex_force_service_name = ex_force_service_name
         self._ex_force_service_region = ex_force_service_region
+        self._ex_force_service_region = ex_force_service_region
+        self._ex_domain_name = ex_domain_name
         self._osa = None
 
         if ex_force_auth_token and not ex_force_base_url:
@@ -179,6 +183,7 @@ class OpenStackBaseConnection(ConnectionUserAndKey):
                             user_id=self.user_id,
                             key=self.key,
                             tenant_name=self._ex_tenant_name,
+                            domain_name=self._ex_domain_name,
                             timeout=self.timeout,
                             parent_conn=self)
 
@@ -386,6 +391,7 @@ class OpenStackDriverMixin(object):
         self._ex_force_service_name = kwargs.get('ex_force_service_name', None)
         self._ex_force_service_region = kwargs.get('ex_force_service_region',
                                                    None)
+        self._ex_domain_name = kwargs.get('ex_domain_name', None)
 
     def openstack_connection_kwargs(self):
         """
@@ -409,4 +415,6 @@ class OpenStackDriverMixin(object):
             rv['ex_force_service_name'] = self._ex_force_service_name
         if self._ex_force_service_region:
             rv['ex_force_service_region'] = self._ex_force_service_region
+        if self._ex_domain_name:
+            rv['ex_domain_name'] = self._ex_domain_name
         return rv

411dec6cce1799a3337bbda9ae8004cf1b8e0859
Bjørnar Myrheim <myrheimb@gmail.com>
2021-02-09 10:22:32 +0900

    Update DigitalOcean DNS driver
    The current driver for DigitalOcean fails and we need to implement
    the most recent update from https://github.com/apache/libcloud
    
    Ref: https://github.com/niteoweb/easyblognetworks/issues/2132
    Ref: https://github.com/apache/libcloud/commits/trunk/libcloud/dns/drivers/digitalocean.py

--- libcloud/dns/drivers/digitalocean.py
+++ libcloud/dns/drivers/digitalocean.py
@@ -20,6 +20,8 @@ __all__ = [
     'DigitalOceanDNSDriver'
 ]
 
+import json
+
 from libcloud.utils.py3 import httplib
 
 from libcloud.common.digitalocean import DigitalOcean_v2_BaseDriver
@@ -125,10 +127,10 @@ class DigitalOceanDNSDriver(DigitalOcean_v2_BaseDriver, DNSDriver):
         params = {'name': domain}
         try:
             params['ip_address'] = extra['ip']
-        except:
+        except Exception:
             params['ip_address'] = '127.0.0.1'
 
-        res = self.connection.request('/v2/domains', params=params,
+        res = self.connection.request('/v2/domains', data=json.dumps(params),
                                       method='POST')
 
         return Zone(id=res.object['domain']['name'],
@@ -179,13 +181,17 @@ class DigitalOceanDNSDriver(DigitalOcean_v2_BaseDriver, DNSDriver):
             except KeyError:
                 params['weight'] = 'null'
 
+            if 'ttl' in extra:
+                params['ttl'] = extra['ttl']
+
         res = self.connection.request('/v2/domains/%s/records' % zone.id,
-                                      params=params,
+                                      data=json.dumps(params),
                                       method='POST')
 
         return Record(id=res.object['domain_record']['id'],
                       name=res.object['domain_record']['name'],
                       type=type, data=data, zone=zone,
+                      ttl=res.object['domain_record'].get('ttl', None),
                       driver=self, extra=extra)
 
     def update_record(self, record, name=None, type=None,
@@ -234,14 +240,18 @@ class DigitalOceanDNSDriver(DigitalOcean_v2_BaseDriver, DNSDriver):
             except KeyError:
                 params['weight'] = 'null'
 
+            if 'ttl' in extra:
+                params['ttl'] = extra['ttl']
+
         res = self.connection.request('/v2/domains/%s/records/%s' %
                                       (record.zone.id, record.id),
-                                      params=params,
+                                      data=json.dumps(params),
                                       method='PUT')
 
         return Record(id=res.object['domain_record']['id'],
                       name=res.object['domain_record']['name'],
                       type=record.type, data=data, zone=record.zone,
+                      ttl=res.object['domain_record'].get('ttl', None),
                       driver=self, extra=extra)
 
     def delete_zone(self, zone):
@@ -284,7 +294,8 @@ class DigitalOceanDNSDriver(DigitalOcean_v2_BaseDriver, DNSDriver):
                  'weight': data['weight']}
         return Record(id=data['id'], name=data['name'],
                       type=self._string_to_record_type(data['type']),
-                      data=data['data'], zone=zone, driver=self, extra=extra)
+                      data=data['data'], zone=zone, ttl=data.get('ttl', None),
+                      driver=self, extra=extra)
 
     def _to_zone(self, data):
         extra = {'zone_file': data['zone_file']}
